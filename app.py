bf7b4dfa368b617ae1bd3b5879b7356275d90f86
neusj47 <neusj47@gmail.com>
2021-05-10 00:55:43 +0900

    # initial commit # 기간별, indicator 호출하는 함수

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas_datareader.data as web
+import dash_bootstrap_components as dbc
 import datetime
 from datetime import date
 import pandas as pd
@@ -46,16 +47,24 @@ for i in range(1,len(TICKER)):
 # 데이터타입(Date)변환 문제로 csv 저장 후, 다시 불러옵니다. (파일 경로 설정 필요!!)
 df = df.reset_index().rename(columns={"index": "id"})
 df.to_csv('pricevolume.csv', index=False, encoding='cp949')
-df = pd.read_csv('..../pricevolume.csv')
+df = pd.read_csv('C:/Users/ysj/PycharmProjects/datepicker_chart/pricevolume.csv')
 
 availble_TICKER = df['TICKER'].unique()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0'}]
+                )
 
-app.layout = html.Div([
-    html.H2(children='PRICE-CHART by date, indicator'),
+app.layout = dbc. Container([
+
+    dbc.Row(
+        dbc.Col(html.H1('PRICE-CHART by date, indicator',className = 'text-center text-primary mb-4'), width = 12)
+    ),
+    # 기간이 datapicker range로 입력됩니다.
+    html.Div(children = 'Input Date'),
     dcc.DatePickerRange(
         id='my-date-picker-range',
         min_date_allowed=date(1995, 8, 5),
@@ -65,27 +74,40 @@ app.layout = html.Div([
         end_date=date(2020, 1, 1),
         display_format = 'YYYY/MM/DD'
     ),
+
     html.Div(id='output-container-date-picker-range'),
-    html.Div(children='''    
-    Select your TICKER !!
-    '''),
-    dcc.Dropdown(
-        id='cell-name-TICKER-column',
-        options=[{'label': i, 'value': i} for i in availble_TICKER],
-        value='AAPL'
-    ),
-    dcc.Dropdown(
-        id='cell-name-indicator-column',
-        options=[{'label': s, 'value': s} for s in ['Volume','Return(cum)','Close']],
-        value='Volume'
-    ),
+
+    html.Hr(),
+
+    dbc.Row([
+        # TICKER가 dropdown으로 입력됩니다.
+        dbc.Col([
+            html.Div(children = 'Input your TICKER'),
+            dcc.Dropdown(
+                id='cell-name-TICKER-column',
+                options=[{'label': i, 'value': i} for i in availble_TICKER],
+                value='AAPL')
+        ], xs=12, sm = 12, md = 12, lg =5, xl=5),
+        # Indicator가 dropdown으로 입력됩니다.
+        dbc.Col([
+            html.Div(children = 'Input your Indicator'),
+            dcc.Dropdown(
+                id='cell-name-indicator-column',
+                options=[{'label': s, 'value': s} for s in ['Volume','Return(cum)','Close']],
+                value='Volume'
+            )
+        ], xs=12, sm = 12, md = 12, lg =5, xl=5)
+    ],  no_gutters=False , justify='start' ),
     dcc.Graph(
         style={'height': 500},
         id='my-graph'
     ),
     html.Hr()
-])
+],fluid=True)
 
+# 입력된 Input으로 Output이 만들어지는 Call back 함수입니다.
+# Input : TICKER, INDICATOR, DATE
+# Output : df 중, TICKER, INDICATOR, DATE를 만족시키는 데이터의 그래프
 @app.callback(
     Output('my-graph', 'figure'),
     [Input('cell-name-TICKER-column', 'value'),
